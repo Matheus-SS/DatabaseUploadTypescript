207026029df195b2affd8f259dcc82a9a463d12b
Matheus Santos <matheus_sa_santos@hotmail.com>
2020-05-14 15:53:44 -0300

    finished

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,96 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
+import { getRepository, getCustomRepository, In } from 'typeorm';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface CsvDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(csvFilePath: string): Promise<Transaction[]> {
+    const readCSVStream = fs.createReadStream(csvFilePath);
+
+    const categoryRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transactions: CsvDTO[] = [];
+    const categories: string[] = [];
+
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((word: string) => word);
+
+      transactions.push({ title, type, value, category });
+      categories.push(category);
+    });
+
+    await new Promise(resolve => {
+      parseCSV.on('end', resolve);
+    });
+
+    const categoriesTitleNotRepeated = Array.from(new Set(categories));
+
+    const existingCategories = await categoryRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    const existingCategoriesTitle = existingCategories.map(
+      existingCategory => existingCategory.title,
+    );
+
+    const categoriesNotAdded = categoriesTitleNotRepeated.filter(valor => {
+      let categoryNotAdd;
+      if (existingCategoriesTitle.indexOf(valor) === -1) {
+        categoryNotAdd = valor;
+      }
+      return categoryNotAdd;
+    });
+
+    const newCategories = categoryRepository.create(
+      categoriesNotAdded.map(categoryTitle => ({
+        title: categoryTitle,
+      })),
+    );
+
+    await categoryRepository.save(newCategories);
+
+    const allCategoriesInTransaction = [
+      ...existingCategories,
+      ...newCategories,
+    ];
+
+    const newTransactions = transactionsRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        value: transaction.value,
+        type: transaction.type,
+        category: allCategoriesInTransaction.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+
+    await transactionsRepository.save(newTransactions);
+
+    await fs.promises.unlink(csvFilePath);
+
+    return newTransactions;
   }
 }
 
